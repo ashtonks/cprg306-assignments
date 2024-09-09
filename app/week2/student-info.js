5b189e7414f6db5b6e7faab274204a51cb54ef12
Ashton <kostiukashton@gmail.com>
2024-09-09 09:23:07 -0600

    Finish assignment 2

---
+++ app/week2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return(
+        <main>
+            <h1>Ashton Kostiuk</h1>
+            <Link href="https://github.com/ashtonks/cprg306-assignments">Github</Link>
+        </main>
+    );
+}
